45e9e34e996174bba3f651c233958b9741b4c5b4
FedorichK <fknov2014@gmail.com>
2023-10-18 21:57:41 +0300

    feat: add end game message, add result table, add footer

--- script.js
+++ script.js
@@ -9,6 +9,11 @@ let successSound = new Audio('./sounds/succes.mp3');
 let errorSound = new Audio('./sounds/error.mp3');
 let newGameSound = new Audio('./sounds/new.mp3');
 
+let moveCount = 0;
+let totalPairs = 6;
+
+let results = JSON.parse(localStorage.getItem('results')) || [];
+
 document.getElementById('newGameButton').addEventListener('click', function() {
     newGameSound.play();
     setTimeout(function(){ location.reload(); }, 1500);
@@ -25,10 +30,12 @@ function handleCardClick() {
     if (!hasCardInverted) {
         hasCardInverted = true;
         cardOne = this;
+        moveCount++;
         return;
     }
 
     cardTwo = this;
+    moveCount++;
 
     verifyMatch();
 }
@@ -45,8 +52,49 @@ function disableCards() {
     cardOne.removeEventListener('click', handleCardClick);
     cardTwo.removeEventListener('click', handleCardClick);
 
+    totalPairs--;
+
+    if (totalPairs === 0) {
+        setTimeout(function() {
+            document.querySelector('.memory__cards').style.display = 'none';
+
+            let endGameMessage = document.getElementById('endGameMessage');
+            endGameMessage.textContent = 'Your count is ' + moveCount + ' moves';
+            endGameMessage.style.display = 'block';
+
+            // Сохраняем результат текущей игры
+            results.push(moveCount);
+
+            // Если в массиве больше 10 результатов, удаляем самый старый
+            if (results.length > 10) {
+                results.shift();
+            }
+
+            // Сохраняем массив результатов в Local Storage
+            localStorage.setItem('results', JSON.stringify(results));
+
+            // Отображаем таблицу рекордов
+            displayResults();
+        }, 500);
+    }
+
     resetGameBoard();
 }
+
+function displayResults() {
+    let resultsTable = document.getElementById('resultsTable');
+    resultsTable.innerHTML = '<tr><th>Game</th><th>Moves</th></tr>';
+
+    for (let i = 0; i < results.length; i++) {
+        let row = document.createElement('tr');
+        row.innerHTML = '<td>' + (i + 1) + '</td><td>' + results[i] + '</td>';
+        resultsTable.appendChild(row);
+    }
+
+    resultsTable.style.display = 'block';
+}
+
+
 
 function unInvertCards() {
     boardLocked = true;
@@ -58,6 +106,7 @@ function unInvertCards() {
         resetGameBoard();
     }, 1500);
 }
+
 
 function resetGameBoard() {
     [hasCardInverted, boardLocked] = [false, false];
